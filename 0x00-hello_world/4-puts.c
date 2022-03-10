a01dead44dcaa90c369434b66b8ae0d25c83000e
Aimeshaloom <aimeshaloom5@gmail.com>
2022-03-10 14:00:12 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #!/bin/bash
-puts("Programming is like building a multilingual puzzle");
+#include <stdio.h>
+{
 
-	return (0)
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
+}
